5db34bfbbb045ee1e8c485dca832b97ea0e8fb4b
Aarav8400 <55765837+Aarav8400@users.noreply.github.com>
2020-05-22 14:50:32 +0530

    Create 4.py

---
+++ day1/4.py
@@ -0,0 +1,7 @@
+cp=float(input('enter the cost price = '))
+sp=float(input('enter the selling price = '))
+profit=sp-cp
+
+print('profit is ',profit)
+newsp=sp+5*cp/100
+print('newselling price will be',newsp)
