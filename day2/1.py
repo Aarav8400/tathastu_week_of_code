98c10d8b05de754f4de2639211ea481df182f95f
Aarav8400 <55765837+Aarav8400@users.noreply.github.com>
2020-05-23 08:49:20 +0530

    Update 1.py

--- day2/1.py
+++ day2/1.py
@@ -1,6 +1,6 @@
 # checking number is even or odd..........................
 
-num=int(input('enter a number to check even or odd'))
+num=int(input('enter a number to check even or odd\n'))
 if num%2==0:
     print(f'{num} is even')
 else:
